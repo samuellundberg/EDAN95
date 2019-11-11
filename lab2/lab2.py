2567be7fa6bc54d4685138f453e9658577aa2969
Sofie Hellmark <sofie.hellmark@gmail..com>
2019-11-11 15:00:27 +0100

    started on lab2, continue on 1.3

---
+++ lab2/lab2.py
@@ -0,0 +1,41 @@
+from sklearn import datasets
+import matplotlib.pyplot as plt
+from sklearn import tree
+
+if __name__ == '__main__':
+# 1. Use the code skeleton / snippets provided or the notebook used in the tutorial
+# 	from the first course week to load the digits dataset from the datasets provided
+# 	in SciKitLearn. Inspect the data. What is in there?
+
+# The set contains images of digits and labels
+
+    # Load the digits dataset
+    digits = datasets.load_digits()
+
+    # Display the first digit
+    plt.figure(1, figsize=(3, 3))
+    plt.imshow(digits.images[0], cmap=plt.cm.gray_r, interpolation='nearest')
+    #plt.show()
+    print(digits.data[0])
+    # Display one image for each digit
+    images_and_labels = list(zip(digits.images, digits.target))
+    for index, (image, label) in enumerate(images_and_labels[:10]):
+        plt.subplot(2, 5, index + 1)
+        plt.axis('off')
+        plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+        plt.title('Training: %i' % label)
+    #plt.show()
+
+#	2. Split your data set into 70% training data (features and labels), and 30% test 	data.
+
+    split = int(len(digits.data)*0.7)
+
+    training_feature = digits.data[:split]
+    test_feature = digits.data[split+1:]
+    training_labels = digits.target[:split]
+    test_labels = digits.target[split + 1:]
+
+# 3. Set up a DecisionTreeClassifier as it comes in SciKitLearn.
+# TODO: Follow the tutorial (or the respective documentation) and produce a 	plot of the tree with graphviz. What can you learn from this about how the used 	algorithm handles the data?
+    classifier = tree.DecisionTreeClassifier()
+    classifier = classifier.fit(training_feature, training_labels)
