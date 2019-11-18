ef668fdd8b6a81d5f8be4b8e7eb3be0bd60d60df
Samuel Lundberg <samuel@Samuels-MacBook-Air.local>
2019-11-18 17:00:14 +0100

    completing lab2

--- lab2/Handout_SkeletonDT/main.py
+++ lab2/Handout_SkeletonDT/main.py
@@ -2,7 +2,7 @@ import ToyData as td
 import ID3
 
 import numpy as np
-from sklearn import tree, metrics, datasets
+# from sklearn import tree, metrics, datasets
 
 
 def main():
@@ -18,8 +18,10 @@ def main():
     print(myTree)
     plot = id3.make_dot_data()
     plot.render("testTree")
-    predicted = id3.predict(data2, myTree)
-    print(predicted)
+    predicted = id3.predict(data2, myTree, attributes)
+    print('predicted: ', predicted)
+    print('data2: ', data2)
+    print('target2: ', target2)
 
 
 if __name__ == "__main__": main()
